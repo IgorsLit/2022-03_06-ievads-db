09cfba473148304e250c36989d24758d964d3732
IgorsLit <igors.litvjakovs@rvt.lv>
2022-05-07 12:36:35 +0300

    Lesson 4, Databases Tables creation, 3 example (2022-7-mysql-tema04.pdf) - CREATE INDEX, added FOREIGN KEY.

--- 04les_example03.sql
+++ 04les_example03.sql
@@ -103,3 +103,152 @@ DB [sakila]> SELECT * FROM film WHERE release_year=2006 AND rental_rate>0.99 AND
 5 rows in set (0.095 sec)
 
 DB [sakila]> EXIT;
+
+
+List of all client commands:
+Note that all text commands must be first on line and end with ';'
+?         (\?) Synonym for `help'.
+clear     (\c) Clear the current input statement.
+connect   (\r) Reconnect to the server. Optional arguments are db and host.
+delimiter (\d) Set statement delimiter.
+edit      (\e) Edit command with $EDITOR.
+ego       (\G) Send command to DB server, display result vertically.
+exit      (\q) Exit mysql. Same as quit.
+go        (\g) Send command to DB server.
+help      (\h) Display this help.
+nopager   (\n) Disable pager, print to stdout.
+notee     (\t) Don't write into outfile.
+pager     (\P) Set PAGER [to_pager]. Print the query results via PAGER.
+print     (\p) Print current command.
+prompt    (\R) Change your mysql prompt.
+quit      (\q) Quit mysql.
+rehash    (\#) Rebuild completion hash.
+source    (\.) Execute an SQL script file. Takes a file name as an argument.
+status    (\s) Get status information from the server.
+system    (\!) Execute a system shell command.
+tee       (\T) Set outfile [to_outfile]. Append everything into given outfile.
+use       (\u) Use another database. Takes database name as argument.
+charset   (\C) Switch to another charset. Might be needed for processing binlog with multi-byte charsets.
+warnings  (\W) Show warnings after every statement.
+nowarning (\w) Don't show warnings after every statement.
+
+For server side help, type 'help contents'
+
+
+DB [(none)]> source /home/user/dic1/file.sql
+Query OK, 0 rows affected (0.031 sec)
+
+Database changed
+
+Query OK, 0 rows affected (0.159 sec)
+Records: 0  Duplicates: 0  Warnings: 0
+
+DB [none]> USE databasestablescreation;
+
+Database changed
+DB [databasestablescreation]> CREATE TABLE IF NOT EXISTS films (
+    ->  country_id TINYINT(2) UNSIGNED PRIMARY KEY AUTO_INCREMENT,  name VARCHAR(25) NOT NULL DEFAULT '');
+Query OK, 0 rows affected (0.157 sec)
+
+DB [databasestablescreation]> ALTER TABLE films RENAME COLUMN country_id TO film_id;
+Query OK, 0 rows affected (0.608 sec)
+Records: 0  Duplicates: 0  Warnings: 0
+
+DB [databasestablescreation]> ALTER TABLE films RENAME COLUMN name TO title;
+Query OK, 0 rows affected (0.058 sec)
+Records: 0  Duplicates: 0  Warnings: 0
+
+DB [databasestablescreation]> CREATE TABLE languages (
+    -> name VARCHAR(10) NOT NULL UNIQUE,
+    -> last_update TIMESTAMP);
+Query OK, 0 rows affected (0.513 sec)
+
+DB [databasestablescreation]> DESCRIBE films;
++---------+---------------------+------+-----+---------+----------------+
+| Field   | Type                | Null | Key | Default | Extra          |
++---------+---------------------+------+-----+---------+----------------+
+| film_id | tinyint(2) unsigned | NO   | PRI | NULL    | auto_increment |
+| title   | varchar(25)         | NO   |     |         |                |
++---------+---------------------+------+-----+---------+----------------+
+2 rows in set (0.221 sec)
+
+DB [databasestablescreation]> DESCRIBE languages;
++-------------+-------------+------+-----+---------------------+-------------------------------+
+| Field       | Type        | Null | Key | Default             | Extra                         |
++-------------+-------------+------+-----+---------------------+-------------------------------+
+| name        | varchar(10) | NO   | PRI | NULL                |                               |
+| last_update | timestamp   | NO   |     | current_timestamp() | on update current_timestamp() |
++-------------+-------------+------+-----+---------------------+-------------------------------+
+2 rows in set (0.019 sec)
+
+DB [databasestablescreation]> ALTER TABLE languages ADD language_id TINYINT(1) UNSIGNED PRIMARY KEY AUTO_INCREMENT;
+Query OK, 0 rows affected (0.179 sec)
+Records: 0  Duplicates: 0  Warnings: 0
+
+DB [databasestablescreation]> DESCRIBE languages;
++-------------+---------------------+------+-----+---------------------+-------------------------------+
+| Field       | Type                | Null | Key | Default             | Extra                         |
++-------------+---------------------+------+-----+---------------------+-------------------------------+
+| name        | varchar(10)         | NO   | UNI | NULL                |                               |
+| last_update | timestamp           | NO   |     | current_timestamp() | on update current_timestamp() |
+| language_id | tinyint(1) unsigned | NO   | PRI | NULL                | auto_increment                |
++-------------+---------------------+------+-----+---------------------+-------------------------------+
+3 rows in set (0.078 sec)
+
+DB [databasestablescreation]> ALTER TABLE films ADD COLUMN language_id TINYINT(2) UNSIGNED;
+Query OK, 0 rows affected (0.074 sec)
+Records: 0  Duplicates: 0  Warnings: 0
+
+DB [databasestablescreation]> DESCRIBE films;
++-------------+---------------------+------+-----+---------+-------+
+| Field       | Type                | Null | Key | Default | Extra |
++-------------+---------------------+------+-----+---------+-------+
+| film_id     | tinyint(1) unsigned | NO   | PRI | NULL    |       |
+| title       | varchar(25)         | NO   |     |         |       |
+| language_id | tinyint(2) unsigned | YES  |     | NULL    |       |
++-------------+---------------------+------+-----+---------+-------+
+3 rows in set (0.027 sec)
+
+DB [databasestablescreation]> ALTER TABLE films ADD CONSTRAINT fk_language_id FOREIGN KEY(language_id) REFERENCES languages(language_id);
+Query OK, 0 rows affected (0.332 sec)
+Records: 0  Duplicates: 0  Warnings: 0
+
+DB [databasestablescreation]> DESCRIBE films;
++-------------+---------------------+------+-----+---------+-------+
+| Field       | Type                | Null | Key | Default | Extra |
++-------------+---------------------+------+-----+---------+-------+
+| film_id     | tinyint(1) unsigned | NO   | PRI | NULL    |       |
+| title       | varchar(25)         | NO   |     |         |       |
+| language_id | tinyint(2) unsigned | YES  | MUL | NULL    |       |
++-------------+---------------------+------+-----+---------+-------+
+3 rows in set (0.021 sec)
+
+DB [databasestablescreation]> DESCRIBE languages;
++-------------+---------------------+------+-----+---------------------+-------------------------------+
+| Field       | Type                | Null | Key | Default             | Extra                         |
++-------------+---------------------+------+-----+---------------------+-------------------------------+
+| name        | varchar(10)         | NO   | UNI | NULL                |                               |
+| last_update | timestamp           | NO   |     | current_timestamp() | on update current_timestamp() |
+| language_id | tinyint(1) unsigned | NO   | PRI | NULL                | auto_increment                |
++-------------+---------------------+------+-----+---------------------+-------------------------------+
+3 rows in set (0.025 sec)
+
+DB [databasestablescreation]> SHOW INDEX FROM films;
++-------+------------+----------------+--------------+-------------+-----------+-------------+----------+--------+------+------------+---------+---------------+
+| Table | Non_unique | Key_name       | Seq_in_index | Column_name | Collation | Cardinality | Sub_part | Packed | Null | Index_type | Comment | Index_comment |
++-------+------------+----------------+--------------+-------------+-----------+-------------+----------+--------+------+------------+---------+---------------+
+| films |          0 | PRIMARY        |            1 | film_id     | A         |           0 |     NULL | NULL   |      | BTREE      |         |               |
+| films |          1 | fk_language_id |            1 | language_id | A         |           0 |     NULL | NULL   | YES  | BTREE      |         |               |
++-------+------------+----------------+--------------+-------------+-----------+-------------+----------+--------+------+------------+---------+---------------+
+2 rows in set (0.018 sec)
+
+DB [databasestablescreation]> SHOW INDEX FROM languages;
++-----------+------------+----------+--------------+-------------+-----------+-------------+----------+--------+------+------------+---------+---------------+
+| Table     | Non_unique | Key_name | Seq_in_index | Column_name | Collation | Cardinality | Sub_part | Packed | Null | Index_type | Comment | Index_comment |
++-----------+------------+----------+--------------+-------------+-----------+-------------+----------+--------+------+------------+---------+---------------+
+| languages |          0 | PRIMARY  |            1 | language_id | A         |           0 |     NULL | NULL   |      | BTREE      |         |               |
+| languages |          0 | name     |            1 | name        | A         |           0 |     NULL | NULL   |      | BTREE      |         |               |
++-----------+------------+----------+--------------+-------------+-----------+-------------+----------+--------+------+------------+---------+---------------+
+2 rows in set (0.008 sec)
+
+DB [databasestablescreation]> EXIT;
