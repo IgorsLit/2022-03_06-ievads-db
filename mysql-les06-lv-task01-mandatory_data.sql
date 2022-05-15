89da58e0f58e34d4ff689d94042a04478f705832
IgorsLit <igors.litvjakovs@rvt.lv>
2022-05-15 19:31:33 +0300

    Lesson 6, Data Bases and tables maintenance,
    task 1, data files (*_data*.sql)

---
+++ mysql-les06-lv-task01-mandatory_data.sql
@@ -0,0 +1,6 @@
+-- --------------------------------------------------------------------------
+-- Insert information
+-- --------------------------------------------------------------------------
+INSERT INTO tests61 (name, theme) VALUES('1. kontroldarbs', 'Mainiigie'), ('2. kontroldarbs', 'Ievade, izvade');
+
+INSERT INTO tasks61 (theme, type, task_text) VALUES('Mainigie', 'Daudzizveellu', 'Izvelies mainigu tipus'), ('Mainigie', 'Daudzizveellu', 'Kas ir integer?'), ('Mainigie', 'Daudzizveellu', 'Kas ir float?'), ('Ievade, izvade', 'Garais teksts', 'Pieraksti programmas kodu, kura jaievdada divus veselus skaitlus.' ), ('Ievade, izvade','Garais teksts', 'Kas ir ievade?'), ('Ievade, izvade', 'Daudzdzizveellu', 'Izvelies kas ir izvade?');
