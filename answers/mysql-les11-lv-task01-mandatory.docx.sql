189353860ec9c1aa6126abcf11808a047e6b95d1
IgorsLit <igors.litvjakovs@rvt.lv>
2022-06-14 00:33:46 +0300

    Lesson 11, subQueries, task1 *.sql - 2 part, updated *README.md

--- answers/mysql-les11-lv-task01-mandatory.docx.sql
+++ answers/mysql-les11-lv-task01-mandatory.docx.sql
@@ -265,3 +265,262 @@ mysql> SELECT DISTINCT title FROM employee_title_vw AS oute
 4 rows in set (0.029 sec)
 
 mysql> exit
+
+Welcome to the MySql monitor.  Commands end with ; or \g.
+Your MySql connection id is 3
+
+Type 'help;' or '\h' for help. Type '\c' to clear the current input statement.
+
+mysql> USE employees;
+Reading table information for completion of table and column names
+You can turn off this feature to get a quicker startup with -A
+
+Database changed
+mysql> SHOW TABLES;
++----------------------+
+| Tables_in_employees  |
++----------------------+
+| current_dept_emp     |
+| departments          |
+| dept_emp             |
+| dept_emp_latest_date |
+| dept_manager         |
+| employee_title_vw    |
+| employees            |
+| salaries             |
+| titles               |
++----------------------+
+9 rows in set (0.004 sec)
+
+mysql> SELECT * FROM salaries LIMIT 10;
++--------+--------+------------+------------+
+| emp_no | salary | from_date  | to_date    |
++--------+--------+------------+------------+
+|  10001 |  60117 | 1986-06-26 | 1987-06-26 |
+|  10001 |  62102 | 1987-06-26 | 1988-06-25 |
+|  10001 |  66074 | 1988-06-25 | 1989-06-25 |
+|  10001 |  66596 | 1989-06-25 | 1990-06-25 |
+|  10001 |  66961 | 1990-06-25 | 1991-06-25 |
+|  10001 |  71046 | 1991-06-25 | 1992-06-24 |
+|  10001 |  74333 | 1992-06-24 | 1993-06-24 |
+|  10001 |  75286 | 1993-06-24 | 1994-06-24 |
+|  10001 |  75994 | 1994-06-24 | 1995-06-24 |
+|  10001 |  76884 | 1995-06-24 | 1996-06-23 |
++--------+--------+------------+------------+
+10 rows in set (0.054 sec)
+
+mysql> SELECT * FROM salaries WHERE emp_no = 10001;
++--------+--------+------------+------------+
+| emp_no | salary | from_date  | to_date    |
++--------+--------+------------+------------+
+|  10001 |  60117 | 1986-06-26 | 1987-06-26 |
+|  10001 |  62102 | 1987-06-26 | 1988-06-25 |
+|  10001 |  66074 | 1988-06-25 | 1989-06-25 |
+|  10001 |  66596 | 1989-06-25 | 1990-06-25 |
+|  10001 |  66961 | 1990-06-25 | 1991-06-25 |
+|  10001 |  71046 | 1991-06-25 | 1992-06-24 |
+|  10001 |  74333 | 1992-06-24 | 1993-06-24 |
+|  10001 |  75286 | 1993-06-24 | 1994-06-24 |
+|  10001 |  75994 | 1994-06-24 | 1995-06-24 |
+|  10001 |  76884 | 1995-06-24 | 1996-06-23 |
+|  10001 |  80013 | 1996-06-23 | 1997-06-23 |
+|  10001 |  81025 | 1997-06-23 | 1998-06-23 |
+|  10001 |  81097 | 1998-06-23 | 1999-06-23 |
+|  10001 |  84917 | 1999-06-23 | 2000-06-22 |
+|  10001 |  85112 | 2000-06-22 | 2001-06-22 |
+|  10001 |  85097 | 2001-06-22 | 2002-06-22 |
+|  10001 |  88958 | 2002-06-22 | 9999-01-01 |
++--------+--------+------------+------------+
+17 rows in set (0.016 sec)
+
+mysql> SELECT * FROM salaries WHERE emp_no = 10001 AND YEAR(to_date) = 9999;
++--------+--------+------------+------------+
+| emp_no | salary | from_date  | to_date    |
++--------+--------+------------+------------+
+|  10001 |  88958 | 2002-06-22 | 9999-01-01 |
++--------+--------+------------+------------+
+1 row in set (0.012 sec)
+
+mysql> SELECT * FROM salaries WHERE emp_no = 10002 AND YEAR(to_date) = 9999;
++--------+--------+------------+------------+
+| emp_no | salary | from_date  | to_date    |
++--------+--------+------------+------------+
+|  10002 |  72527 | 2001-08-02 | 9999-01-01 |
++--------+--------+------------+------------+
+1 row in set (0.011 sec)
+
+mysql> SELECT * FROM dept_emp LIMIT 10;
++--------+---------+------------+------------+
+| emp_no | dept_no | from_date  | to_date    |
++--------+---------+------------+------------+
+|  10001 | d005    | 1986-06-26 | 9999-01-01 |
+|  10002 | d007    | 1996-08-03 | 9999-01-01 |
+|  10003 | d004    | 1995-12-03 | 9999-01-01 |
+|  10004 | d004    | 1986-12-01 | 9999-01-01 |
+|  10005 | d003    | 1989-09-12 | 9999-01-01 |
+|  10006 | d005    | 1990-08-05 | 9999-01-01 |
+|  10007 | d008    | 1989-02-10 | 9999-01-01 |
+|  10008 | d005    | 1998-03-11 | 2000-07-31 |
+|  10009 | d006    | 1985-02-18 | 9999-01-01 |
+|  10010 | d004    | 1996-11-24 | 2000-06-26 |
++--------+---------+------------+------------+
+10 rows in set (0.014 sec)
+
+mysql> SELECT * FROM employees LIMIT 10;
++--------+------------+------------+-----------+--------+------------+
+| emp_no | birth_date | first_name | last_name | gender | hire_date  |
++--------+------------+------------+-----------+--------+------------+
+|  10001 | 1953-09-02 | Georgi     | Facello   | M      | 1986-06-26 |
+|  10002 | 1964-06-02 | Bezalel    | Simmel    | F      | 1985-11-21 |
+|  10003 | 1959-12-03 | Parto      | Bamford   | M      | 1986-08-28 |
+|  10004 | 1954-05-01 | Chirstian  | Koblick   | M      | 1986-12-01 |
+|  10005 | 1955-01-21 | Kyoichi    | Maliniak  | M      | 1989-09-12 |
+|  10006 | 1953-04-20 | Anneke     | Preusig   | F      | 1989-06-02 |
+|  10007 | 1957-05-23 | Tzvetan    | Zielinski | F      | 1989-02-10 |
+|  10008 | 1958-02-19 | Saniya     | Kalloufi  | M      | 1994-09-15 |
+|  10009 | 1952-04-19 | Sumant     | Peac      | F      | 1985-02-18 |
+|  10010 | 1963-06-01 | Duangkaew  | Piveteau  | F      | 1989-08-24 |
++--------+------------+------------+-----------+--------+------------+
+10 rows in set (0.011 sec)
+
+mysql> SELECT * FROM departments LIMIT 10;
++---------+--------------------+
+| dept_no | dept_name          |
++---------+--------------------+
+| d009    | Customer Service   |
+| d005    | Development        |
+| d002    | Finance            |
+| d003    | Human Resources    |
+| d001    | Marketing          |
+| d004    | Production         |
+| d006    | Quality Management |
+| d008    | Research           |
+| d007    | Sales              |
++---------+--------------------+
+9 rows in set (0.013 sec)
+
+mysql> SELECT COUNT(*) FROM departments;
++----------+
+| COUNT(*) |
++----------+
+|        9 |
++----------+
+1 row in set (0.009 sec)
+
+mysql> SELECT emp.last_name, emp.emp_no, dep.dept_name, dep.dept_no, de.emp_no, de.dept_no, de.to_date FROM employees AS emp JOIN dept_emp AS de USING(emp_no) JOIN departments AS dep USING(dept_no) WHERE YEAR(de.to_date) = 9999 LIMIT 5;
++-------------+--------+------------------+---------+--------+---------+------------+
+| last_name   | emp_no | dept_name        | dept_no | emp_no | dept_no | to_date    |
++-------------+--------+------------------+---------+--------+---------+------------+
+| Lortz       |  10038 | Customer Service | d009    |  10038 | d009    | 9999-01-01 |
+| Tramer      |  10049 | Customer Service | d009    |  10049 | d009    | 9999-01-01 |
+| Billingsley |  10060 | Customer Service | d009    |  10060 | d009    | 9999-01-01 |
+| Syrzycki    |  10088 | Customer Service | d009    |  10088 | d009    | 9999-01-01 |
+| Swick       |  10112 | Customer Service | d009    |  10112 | d009    | 9999-01-01 |
++-------------+--------+------------------+---------+--------+---------+------------+
+5 rows in set (0.042 sec)
+
+mysql> SELECT dep.dept_name, dep.dept_no, max(sl.salary), de.to_date AS dep_emp_date, sl.to_date AS salary_date FROM employees AS emp JOIN dept_emp AS de USING(emp_no) JOIN departments AS dep USING(dept_no) JOIN salaries AS sl ON (sl.emp_no = de.emp_no) WHERE YEAR(sl.to_date) = 9999 LIMIT 2;
++------------------+---------+----------------+--------------+-------------+
+| dept_name        | dept_no | max(sl.salary) | dep_emp_date | salary_date |
++------------------+---------+----------------+--------------+-------------+
+| Customer Service | d009    |         158220 | 9999-01-01   | 9999-01-01  |
++------------------+---------+----------------+--------------+-------------+
+1 row in set (7 min 46.884 sec)
+
+mysql> DROP VIEW dept_max_salary_vw;
+Query OK, 0 rows affected (0.031 sec)
+
+MariaDB [employees]> CREATE VIEW dept_max_salary_vw AS
+    -> SELECT dep.dept_name, dep.dept_no, max(sl.salary) AS max_salary
+    -> FROM departments AS dep
+    -> JOIN dept_emp AS de USING(dept_no)
+    -> JOIN employees AS emp USING(emp_no)
+    -> JOIN salaries AS sl ON (sl.emp_no = de.emp_no)
+    -> WHERE YEAR(sl.to_date) = 9999
+    -> GROUP BY dep.dept_name, dep.dept_no;
+Query OK, 0 rows affected (0.051 sec)
+
+mysql> SELECT CONCAT(first_name, ' ', last_name) AS full_name, (SELECT CONCAT(dept_name, ' ', max_salary) FROM dept_max_salary_vw WHERE dept_no = de.dept_no) FROM employees AS emp JOIN dept_emp AS de USING(emp_no) WHERE YEAR(de.to_date) = 9999 GROUP BY CONCAT(first_name, ' ', last_name);
++--------------------------------+------------------------------------------------------------------------------------------------+
+| full_name                      | (SELECT CONCAT(dept_name, ' ', max_salary) FROM dept_max_salary_vw WHERE dept_no = de.dept_no) |
++--------------------------------+------------------------------------------------------------------------------------------------+
+| Aamer Anger                    | Customer Service 144866                                                                        |
+| Aamer Armand                   | Quality Management 132103                                                                      |
+| Aamer Azevdeo                  | Customer Service 144866                                                                        |
+| Aamer Azuma                    | Quality Management 132103                                                                      |
+| Aamer Baak                     | Sales 158220                                                                                   |
+| Aamer Baaleh                   | Development 144434                                                                             |
+| Aamer Baar                     | Production 138273                                                                              |
+| Aamer Baba                     | Sales 158220                                                                                   |
+| Aamer Bahl                     | Human Resources 141953                                                                         |
+| Aamer Bahr                     | Development 144434                                                                             |
+| Aamer Basawa                   | Marketing 145128                                                                               |
+| Aamer Beausoleil               | Development 144434                                                                             |
+| Aamer Belinskaya               | Development 144434                                                                             |
+| Aamer Benantar                 | Quality Management 132103                                                                      |
+| Aamer Bennis                   | Development 144434                                                                             |
+| Aamer Berendt                  | Development 144434                                                                             |
+| Aamer Beznosov                 | Production 138273                                                                              |
+| Aamer Bharadwaj                | Development 144434                                                                             |
+| Aamer Birch                    | Production 138273                                                                              |
+| Aamer Bottner                  | Finance 142395                                                                                 |
+| Aamer Bouloucos                | Quality Management 132103                                                                      |
+| Aamer Brandsma                 | Development 144434                                                                             |
+| Aamer Bridgland                | Sales 158220                                                                                   |
+| Aamer Brlek                    | Quality Management 132103                                                                      |
+| Aamer Burnard                  | Research 130211                                                                                |
+| Aamer Calkin                   | Development 144434                                                                             |
+| Aamer Cardazo                  | Production 138273                                                                              |
+| Aamer Chenoweth                | Development 144434                                                                             |
+| Aamer Cheshire                 | Production 138273                                                                              |
+| Aamer Conti                    | Sales 158220                                                                                   |
+| Aamer Crabtree                 | Development 144434                                                                             |
+| Aamer Cromarty                 | Sales 158220                                                                                   |
+| Aamer Cummings                 | Production 138273                                                                              |
+| Aamer Cyre                     | Customer Service 144866                                                                        |
+| Aamer Daescu                   | Production 138273                                                                              |
+| Aamer Danecki                  | Production 138273                                                                              |
+| Aamer Danner                   | Marketing 145128                                                                               |
+| Aamer Dehkordi                 | Production 138273                                                                              |
+| Aamer Demke                    | Production 138273                                                                              |
+| Aamer Denny                    | Customer Service 144866                                                                        |
+| Aamer Dulli                    | Quality Management 132103                                                                      |
+| Aamer Dusink                   | Development 144434                                                                             |
+
+
+| Yishai Zaumen                  | Production 138273                                                                              |
+| Yishai Zedlitz                 | Finance 142395                                                                                 |
+| Yishay Aamodt                  | Sales 158220                                                                                   |
+| Yishay Akazan                  | Sales 158220                                                                                   |
+| Yishay Akiyama                 | Human Resources 141953                                                                         |
+| Yishay Alencar                 | Customer Service 144866                                                                        |
+| Yishay Ambroszkiewicz          | Marketing 145128                                                                               |
+| Yishay Auyong                  | Customer Service 144866                                                                        |
+| Yishay Axelband                | Development 144434                                                                             |
+| Yishay Azadmanesh              | Quality Management 132103                                                                      |
+
+
+
+| Zvonko Valtorta                | Production 138273                                                                              |
+| Zvonko Varman                  | Development 144434                                                                             |
+| Zvonko Vecchio                 | Customer Service 144866                                                                        |
+| Zvonko Veeraraghavan           | Production 138273                                                                              |
+| Zvonko Ventosa                 | Marketing 145128                                                                               |
+| Zvonko Vesna                   | Sales 158220                                                                                   |
+| Zvonko Viele                   | Production 138273                                                                              |
+| Zvonko Vigier                  | Finance 142395                                                                                 |
+| Zvonko Waterhouse              | Production 138273                                                                              |
+| Zvonko Whitcomb                | Sales 158220                                                                                   |
+| Zvonko Wielonsky               | Quality Management 132103                                                                      |
+| Zvonko Wilharm                 | Marketing 145128                                                                               |
+| Zvonko Wroclawski              | Development 144434                                                                             |
+| Zvonko Yetto                   | Marketing 145128                                                                               |
+| Zvonko Yoshizawa               | Production 138273                                                                              |
+| Zvonko Yurov                   | Sales 158220                                                                                   |
+| Zvonko Zambonelli              | Finance 142395                                                                                 |
+| Zvonko Zobel                   | Sales 158220                                                                                   |
+| Zvonko Zuberek                 | Production 138273                                                                              |
++--------------------------------+------------------------------------------------------------------------------------------------+
+226781 rows in set (16 min 39.766 sec)
+
+mysql> exit
--- answers/mysql-les11-lv-task01-mandatory.docx_README.md
+++ answers/mysql-les11-lv-task01-mandatory.docx_README.md
@@ -8,6 +8,6 @@ Apakšvaicājumi mēdz būt ļoti noderīgi SQL kodā. Bieži tie var būt aizvi
 
 - [x] Pieslēdzieties MySQL serverim, employees datubāzei.
 - [x] Uzrakstiet vaicājumu, kurš atgriezis visus departamenta vadītāju amatu nosaukumus. Izmantojiet apakšvaicājumu EXISTS. Pamainiet uz apakšvaicājumu IN.
-- [ ] Uzrakstiet vaicājumu, kurš atgriezis visus darbiniekus kopā ar departamenta nosaukumu, kā arī lielāku algu departamentā. Izmantojiet apakšvaicājumu SELECT daļā.
+- [x] Uzrakstiet vaicājumu, kurš atgriezis visus darbiniekus kopā ar departamenta nosaukumu, kā arī lielāku algu departamentā. Izmantojiet apakšvaicājumu SELECT daļā.
 - [ ] Uzrakstiet vaicājumu ar visiem departamentiem, kā arī minimālo, maksimālo, vidējo algu, un darbinieku skaitu. Izmatojiet apakšvaicājumu FROM daļā.
 Saglabājiet savu kodu failā. Failu saglabājiet pasniedzēja norādītajā vietā.
